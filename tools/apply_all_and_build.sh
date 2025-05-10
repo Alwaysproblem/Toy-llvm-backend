e5eecc09836bf9814fe36f218996e9b609203493
yongxi.yang <yongxi.yang.xh@renesas.com>
2025-05-10 15:00:45 +0800

    Tested patch

--- tools/apply_all_and_build.sh
+++ tools/apply_all_and_build.sh
@@ -8,23 +8,23 @@ if [[ -f "/usr/bin/git" ]]; then
 fi
 
 bash ${WORKSPACE}/tools/apply_patch.sh -p Ch1/0001-Adding-the-new-architecture-to-the-Triple-class.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch1/0003-Targeting-Registration.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch1/0004-Add-minimal-Toy-backend.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch1/0005-Add-ToyMCCodeEmitter-and-asm-parser.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch1/0006-Add-disassembler.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch2/0007-Adding-Instruction-Selection.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch2/0009-store-and-load.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0010-find-global-address.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0011-stack-management.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0012-set-less-than-riscv.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0013-add-branch.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0014-support-fp-inst.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0015-support-fma.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0016-support-store-in-asmparser.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0017-write-to-obj.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0018-add-intrinsic-function.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0019-add-intrinsic-function-with-LowerINTRINSIC_WO_CHAIN.patch
-bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0020-fixup-for-branch.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch1/0002-Targeting-Registration.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch1/0003-Add-minimal-Toy-backend.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch1/0004-Add-ToyMCCodeEmitter-and-asm-parser.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch1/0005-Add-disassembler.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch2/0006-Adding-Instruction-Selection.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch2/0007-store-and-load.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0008-find-global-address.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0009-stack-management.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0010-set-less-than-riscv.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0011-add-branch.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0012-support-fp-inst.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0013-support-fma.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0014-support-store-in-asmparser.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0015-write-to-obj.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0016-add-intrinsic-function.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0017-add-intrinsic-function-with-LowerINTRINSIC_WO_C.patch
+bash ${WORKSPACE}/tools/apply_patch.sh -p Ch3/0018-fixup-for-branch.patch
 bash ${WORKSPACE}/tools/build_llc.sh
 
 ./llvm-project/llvm/build/bin/llc vscode/test/hello.ll
